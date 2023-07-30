aad4e95aa2ae20a7d62ecf055c5903cde41f8901
alexc <alexanderjcornish@gmail.com>
2023-07-29 23:24:50 -0400

    Default IntelliJ code

---
+++ src/main/java/com/springwebsite/springwebsitenew/SpringwebsitenewApplication.java
@@ -0,0 +1,13 @@
+package com.springwebsite.springwebsitenew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwebsitenewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwebsitenewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springwebsite/springwebsitenew/SpringwebsitenewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springwebsite.springwebsitenew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwebsitenewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
